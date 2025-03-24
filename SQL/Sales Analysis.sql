72f64685102efa82d7464cac0fe3aa9a483d42c9
BoonchanokWannapanich <boonchanok_dew@hotmail.com>
2025-03-24 16:23:05 +0700

    Update Sales Analysis.sql

--- SQL/Sales Analysis.sql
+++ SQL/Sales Analysis.sql
@@ -24,11 +24,6 @@ FROM DATASuperstore
 GROUP BY Category
 ORDER BY AVG_Profit DESC;
 
-| Column 1 | Column 2 | Column 3 |
-|----------|----------|----------|
-| Value 1  | Value 2  | Value 3  |
-| Value 4  | Value 5  | Value 6  |
-
 -- Which products have the highest sales?
 SELECT Sub_Category , sum(Sales) AS Total_Sales
 FROM DATASuperstore
