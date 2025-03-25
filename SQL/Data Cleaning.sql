b689b7727580152152303a9ebf691c87850ee98b
BoonchanokWannapanich <boonchanok_dew@hotmail.com>
2025-03-26 00:23:10 +0700

    Update Data Cleaning.sql

--- SQL/Data Cleaning.sql
+++ SQL/Data Cleaning.sql
@@ -14,7 +14,20 @@ ALTER TABLE Datasuperstore RENAME COLUMN Sales TO 'Sales($)';
 ALTER TABLE Datasuperstore RENAME COLUMN Discount TO 'Discount(%)';
 ALTER TABLE Datasuperstore RENAME COLUMN Profit TO 'Profit($)'
   
+  
 -- Data Cleaning : Check for missing data, specifically focusing on key data such as Sales, Quantity, Discounts, and Profits.
 SELECT Sales , Quantity , Discount , Profit
 FROM DATASuperstore
 WHERE 'Sales' IS NULL OR 'Quantity' IS NULL OR 'Discount' IS NULL OR 'Profit' IS NULL;
+
+
+-- Data Cleaning : Check for date types are sorted by date/month/year or month/day/year.
+-- Method 1 : sequence view on  data 10 OrderDate to see the date sort order.
+SELECT DISTINCT OrderDate
+FROM DATASuperstore
+LIMIT 10;
+
+-- Method 2 : Check if there are more than 12 days because if the first number (month) is more than 12, it is DD/MM/YYYY.
+SELECT OrderDate
+FROM DATASuperstore
+WHERE CAST(substr(OrderDate,1,2) AS INTEGER) > 12;
